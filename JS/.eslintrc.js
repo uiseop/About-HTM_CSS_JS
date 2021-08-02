4f9ad359ab153628c788d16cfe4a06f1c7a3e332
uiseop <bono521@naver.com>
2021-08-02 18:53:03 +0900

    JS이론 학습지 추가

---
+++ JS/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2017": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 8,
+        "sourceType": "module"
+    },
+    "plugins": [
+    ],
+    "rules": {
+    }
+};
--- JS/gulpfile.js
+++ JS/gulpfile.js
@@ -1,11 +1,15 @@
 const gulp = require('gulp')
 // 걸프 의존성을 작성한거
 const babel = require('gulp-babel')
+const eslint = require('gulp-eslint')
 
 // 개발 과정의 반복 작업을 자동화하는 빌드 도구가 걸프래
 
-gulp.task('default',function(){
+gulp.task('default',async function(){
     // 걸프 작업을 여기 쓴데.
+    gulp.src(["es6/**/*.js","public/**/*.js"])
+        .pipe(eslint())
+        .pipe(eslint.format())
     //es6디렉터리의 모든 .js파일들 **는 서브디렉터리를 포함해 모든 디렉터리를 파이프로 연결
     // 노드 소스
     gulp.src("es6/**/*.js")
@@ -16,4 +20,4 @@ gulp.task('default',function(){
         .pipe(babel())
         .pipe(gulp.dest("public/dist"))
 
-})
+});
