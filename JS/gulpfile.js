c4d120a8ef1d3a09ba07190427cfba5c847879d2
uiseop <bono521@naver.com>
2021-08-02 16:15:46 +0900

    gulp를 통해 es6+ -> es5코드 변환 프로젝트 기본 설정

--- JS/gulpfile.js
+++ JS/gulpfile.js
@@ -1,8 +1,19 @@
 const gulp = require('gulp')
 // 걸프 의존성을 작성한거
+const babel = require('gulp-babel')
 
 // 개발 과정의 반복 작업을 자동화하는 빌드 도구가 걸프래
 
 gulp.task('default',function(){
     // 걸프 작업을 여기 쓴데.
+    //es6디렉터리의 모든 .js파일들 **는 서브디렉터리를 포함해 모든 디렉터리를 파이프로 연결
+    // 노드 소스
+    gulp.src("es6/**/*.js")
+        .pipe(babel())
+        .pipe(gulp.dest("dist"))
+    // 브라우저 소스
+    gulp.src("public/es6/**/*.js")
+        .pipe(babel())
+        .pipe(gulp.dest("public/dist"))
+
 })
