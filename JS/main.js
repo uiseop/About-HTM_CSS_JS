79e1a926e077bab6d371682078aa88e9a94c18d0
uiseop <bono521@naver.com>
2021-08-02 14:53:41 +0900

    .gitignore 추가

--- JS/main.js
+++ JS/main.js
@@ -4,11 +4,13 @@ $(document).ready(function(){
     console.log('main.js loaded');
     paper.install(window)
     paper.setup(document.getElementById('mainCanvas'))
-    var c;
-    for(var x=25; x<400; x+=50){
-        for(var y=25; y<400; y+=50){
-            c = Shape.Circle(x,y,20);
-            c.fillColor = 'green'
-        }
+    
+    var tool = new Tool();
+    // Paper.js는 툴tool 객체를 통해 사용자의 입력을 처리
+    // 객체는 이벤트 핸들러를 연결 가능 onMouseDown 이벤트핸들러를 연결
+    tool.onMouseDown = function(event){
+        // var c = Shape.Circle(event.point.x, event.point.y,20)
+        var c = Shape.Circle(event.point, 20)
+        c.fillColor = 'red'
     }
 })
