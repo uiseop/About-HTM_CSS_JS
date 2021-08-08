e72611718a01ef008e8e740e3f58c97e26aa9825
uiseop <bono521@naver.com>
2021-08-08 23:33:44 +0900

    forEach 추가

--- JS/es6/temp.js
+++ JS/es6/temp.js
@@ -1,11 +1,24 @@
-function countdown(){
-    let i
-    console.log("CountDonwn:");
-    for(i=5;i>=0;i--){
-        setTimeout(function(){
-            console.log(i===0?"GO":i);
-        },(5-i)*1000)
+function getAgeAverage(personArray) {
+	var average = 0;
+    for(let person of personArray){
+        console.log(person);
+        average += person["age"]
     }
+
+    average /= personArray.length
+
+	// 연령 평균을 구해주는 함수를 만들어 봅시다.
+	// 평균은 총합을 갯수로 나눠주면 됩니다.
+
+	return average;
 }
 
-countdown()
+var personArray = [
+                    {"name": "John Doe", "age": 20},
+                    {"name": "Jane Doe", "age": 19},
+                    {"name": "Fred Doe", "age": 32},
+                    {"name": "Chris Doe", "age": 45},
+                    {"name": "Layla Doe", "age": 37},
+                ];
+
+console.log(getAgeAverage(personArray)); // 30.6
