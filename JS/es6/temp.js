e1c701198b5f55689e6ab1b3ebb79da0784112ed
uiseop <bono521@naver.com>
2021-08-09 22:38:16 +0900

    callback, DOM, jQuery

--- JS/es6/temp.js
+++ JS/es6/temp.js
@@ -1,24 +1,4 @@
-function getAgeAverage(personArray) {
-	var average = 0;
-    for(let person of personArray){
-        console.log(person);
-        average += person["age"]
-    }
+const input = "As i was going  to saint ives"
+const output = input.replace(/\w{4,}/ig,'****')
 
-    average /= personArray.length
-
-	// 연령 평균을 구해주는 함수를 만들어 봅시다.
-	// 평균은 총합을 갯수로 나눠주면 됩니다.
-
-	return average;
-}
-
-var personArray = [
-                    {"name": "John Doe", "age": 20},
-                    {"name": "Jane Doe", "age": 19},
-                    {"name": "Fred Doe", "age": 32},
-                    {"name": "Chris Doe", "age": 45},
-                    {"name": "Layla Doe", "age": 37},
-                ];
-
-console.log(getAgeAverage(personArray)); // 30.6
+console.log(output);
