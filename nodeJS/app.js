fd8f39a9191829d22696cfa3135253734155f290
uiseop <bono521@naver.com>
2021-08-15 22:18:15 +0900

    Router는 하나의 작은 App이라고도 불린다-길을알려줌

--- nodeJS/app.js
+++ nodeJS/app.js
@@ -3,24 +3,10 @@ const app = express()
 const port = 3000
 
 const goodsRouter = require('./routes/goods.js')
+const userRouter = require('./routes/user.js')
 
 app.use('/goods',goodsRouter)
-
-// app.get('/goods/list', (req, res) => {
-//     res.send('상품 목록 페이지')
-// })
-
-// app.get('/goods/detail', (req, res) => {
-//     res.send('상품 상세 페이지')
-// })
-
-app.get('/user/login', (req, res) => {
-    res.send('로그인 페이지')
-})
-
-app.get('/user/register', (req, res) => {
-    res.send('회원가입 페이지')
-})
+app.use('/user',userRouter)
 
 app.listen(port,()=>{
     console.log(`listening port is ${port}`);
